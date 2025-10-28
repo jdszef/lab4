ce551d6dee131cafa8b6bcd96433b7d08e2ed3d3
Cyprian Partyka <129669957+jdszef@users.noreply.github.com>
2025-10-28 16:38:45 +0100

    lab4 in progress

---
+++ lab4.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+using namespace std;
+//int show(int a, int b){
+//	if (a > b){
+//		return a;
+//	}
+//	else{
+//		return b;
+//	}
+//}
+//
+//void wieksza(int a, int b) {
+//	if (a > b) {
+//		cout<< "Wieksza liczba to: " << a << endl;
+//	}
+//	else {
+//		cout << "Wieksza liczba to: " << b << endl;
+//	}
+//}
+//
+//
+//int main() {
+//	cout << "Podaj dwie liczby calkowite: " << endl;
+//	int a, b;
+//	cin >> a >> b;
+//	wieksza(a, b);
+//	cout << "Funkcja show zwraca wieksza liczbe: " << show(a, b) << endl;
+//			
+//
+//}
+//zadanie 1
+
+//zadanie 2
+	int mkk(int a){
+		if (a % 2 == 0){
+			return a / 2;
+		}
+		else{
+			return 3 * a + 1;
+		}
+	}
+	int main(){
+		cout << "Podaj liczbe calkowita: " << endl;
+		int a;
+		cin >> a;
+		cout << "Wynik funkcji mkk to: " << mkk(a) << endl;
+	
+	}
+
+
+
+
